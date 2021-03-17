ba6da0ad7f10b7aa2726f55082aed2b1d5069bd8
SuanFaRuoJi <gdbzdnql@gmail.com>
2021-03-17 01:41:22 -0700

    Finished raising modulo numbers

--- .scripts/generate_makefile.sh
+++ .scripts/generate_makefile.sh
@@ -9,6 +9,9 @@ TARGET = $1
 $1:
 	\$(CC) -o $1 $1.cpp
 
+test: $1
+  ./$1 < test_data
+
 clean:
 	rm -rf *.o $1
 END
--- raising_module_numbers/raising_module_numbers.cpp
+++ raising_module_numbers/raising_module_numbers.cpp
@@ -1,11 +1,55 @@
 #include <iostream>
 #include <cstdlib>
+#include <string.h>
 
 #include "raising_module_numbers.h"
 
 using namespace std;
 
-int main() {
-  cout << "This problem <raising_module_numbers> hasn't been implemented yet" << endl;
-  return -1;
+bool DEBUG = false;
+
+long long calculate_power_module(long long a, long long b, int p) {
+    long long result = a, total = 1;
+    long long mask = 1;
+    for (; mask <= b; mask <<= 1) {
+        if ((mask & b) != 0) {
+            total = (total * result) % p;
+        }
+        result = (result * result) % p;
+    }
+
+    return total;
 }
+
+int main(int argc, char * argv[]) {
+    int cases;
+    DEBUG = argc >= 2 && strcmp(argv[1], "debug") == 0;
+        cin >> cases;
+        if (DEBUG){
+            cerr << "Total of " << cases << " cases:" << endl;
+        }
+        for (int i=0; i<cases; i++) {
+            if (DEBUG){
+                cerr << "\tCase " << (i+1) << ":" << endl;
+            }
+            long long p, num_players;
+            cin >> p >> num_players;
+
+            if (DEBUG) {
+                cerr << "P: " << p << ", # of players: " << num_players << "." << endl;
+            }
+            long long a, b;
+            int result = 0;
+            for (int j=0; j<num_players; j++) {
+                cin >> a >> b;
+                long long local = calculate_power_module(a % p, b, p);
+                result = (result + local) % p;
+                if (DEBUG) {
+                    cerr << "Player " << (j + 1) << ": " << local << ", current total: " << result << endl;
+                }
+            }
+            cout << result << endl;
+        }
+    return 0;
+}
+
