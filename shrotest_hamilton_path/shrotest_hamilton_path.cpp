1078ba6d7c005d88028e46766973f8bef74333ce
SuanFaRuoJi <gdbzdnql@gmail.com>
2021-03-31 01:01:20 -0700

    Add shorted hamilton path

---
+++ shrotest_hamilton_path/shrotest_hamilton_path.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "shrotest_hamilton_path.h"
+
+using namespace std;
+
+int main() {
+  cout << "This problem <shrotest_hamilton_path> hasn't been implemented yet" << endl;
+  return -1;
+}
