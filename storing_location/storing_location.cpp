d6e2b0dc1902921bcbc368c15c64a88cc27a5879
SuanFaRuoJi <gdbzdnql@gmail.com>
2021-05-02 09:08:54 -0700

    Add storing location

--- storing_location/storing_location.cpp
+++ storing_location/storing_location.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 #include <cstdlib>
+#include <algorithm>
 
 #include "storing_location.h"
 
 using namespace std;
 
+int location[100000];
+
 int main() {
-  cout << "This problem <storing_location> hasn't been implemented yet" << endl;
-  return -1;
+    int N;
+    cin >> N;
+    for (int i=0; i<N; i++) {
+        cin >> location[i];
+    }
+
+    sort(location, location+N);
+
+    int loc = location[N/2];
+
+    long sum = 0;
+    for (int i=0; i<N; i++) {
+        sum += abs(location[i] - loc);
+    }
+
+    cout << sum << endl;
+
+    return 0;
 }
